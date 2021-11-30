f6eeb9e059d6709cacd19c67cadfb40d28a78292
Lien Ber Luen <lienbl@yahoo.com>
2021-11-30 09:37:26 +0800

    update users

--- backend_mysql/routes/users.js
+++ backend_mysql/routes/users.js
@@ -37,6 +37,57 @@ router.post("/add", function (request, response) {
     );
   });
 
+// To add a new user to our database
+router.post("/addw", function (request, response, next) {
+  const accounttype='wallet';
+  const startbalance=50;
+  query(
+    `insert into users (first_name, last_name, mobile, email, login_id, login_password, user_type)
+    values ( 
+      '${request.body.first_name}',
+      '${request.body.last_name}',
+      ${request.body.mobile}, 
+      '${request.body.email}',
+      '${request.body.login_id}',
+      '${request.body.login_password}',
+      '${request.body.user_type}');
+      `, null, (result) => {
+
+        let key;
+        let returnid='';
+
+        for (key in result) {
+          if (key= 'insertId') {
+                  returnid = result[key];
+          }
+        }
+          
+        //const newquery = query(`insert into accounts (owner_id, type) values (${id[0]}, 'wallet')`);
+        //query(newquery, response)
+        //return (`${result[0].inserId[0]} created`)
+
+       query(
+         `INSERT into accounts (owner_id, type, wallet_limit, wallet_balance, restricted_transaction) 
+         VALUES ('${returnid}','${accounttype}','${startbalance}','${startbalance}','null')
+         `, response);
+         console.log('account created');
+         return
+        });
+      //response.status(400).send(result);
+      
+  
+});
+
+/*
+null, (restrictions) => {
+            console.log(restrictions[0].restricted_transaction)
+            if (newrow.category == restrictions[0].restricted_transaction) {
+                console.log('restricted transaction')
+                res.status(400).send('restricted transaction')
+                return
+            }
+*/
+
 // To change user type
 router.get("/change-type", function (request, response) {
     if (!request.query.id) {
